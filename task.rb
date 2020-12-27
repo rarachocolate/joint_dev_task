a61d4638498e6e7ce4feaf7129723c7f3febbfd1
rmk0926 <67753248+rmk0926@users.noreply.github.com>
2020-12-27 17:36:38 +0900

    Q1-Q4

--- task.rb
+++ task.rb
@@ -5,7 +5,10 @@ def q1
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+  names = ["田中", "佐藤", "佐々木", "高橋", "斉藤"]
+  names.each do |name|
+    puts name
+  end
 end
 
 def q2
@@ -13,13 +16,18 @@ def q2
   array2 = ["bird", "bat", "tiger"]
 
   # 以下に回答を記載
+  array = array1 + array2
+  array.each do |animal|
+    puts animal
+  end
 
 end
 
 def q3
   numbers = [1, 5, 8, 10, 2, 3, 2, 3, 3, 1, 4, 5, 9]
-
+  
   # 以下に回答を記載
+  puts numbers.count(3)
 
 end
 
@@ -27,7 +35,7 @@ def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-
+  sports.compact!
   # 以下は変更しないで下さい
   p sports
 end
