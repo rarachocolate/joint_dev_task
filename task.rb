fb285ff23de68eba24e5548f79ccc99e0593525b
rmk0926 <67753248+rmk0926@users.noreply.github.com>
2021-01-18 21:39:52 +0900

    15の修正

--- task.rb
+++ task.rb
@@ -142,17 +142,9 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-  if data1.has_key?(:age)
-    puts "OK"
-  else   
-    puts "NG"
-  end
+  puts data1.has_key?(:age)? "OK" : "NG"
   
-  if data2.has_key?(:age)
-    puts "OK"
-  else   
-    puts "NG"
-  end
+  puts data2.has_key?(:age)? "OK" : "NG"
 end
 
 def q16
