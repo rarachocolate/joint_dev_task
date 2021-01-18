19047f3c64a482e078f785ecc26710b090c8960b
rmk0926 <67753248+rmk0926@users.noreply.github.com>
2021-01-18 15:04:25 +0900

    Q16まで

--- task.rb
+++ task.rb
@@ -117,7 +117,7 @@ def q12
   data = { user: { name: "satou", age: 33 } }
 
   # 以下に回答を記載
-
+  puts data[:user][:name]
 end
 
 def q13
@@ -125,14 +125,16 @@ def q13
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
-
+  user_data.merge!(update_data)
+  puts user_data
 end
 
 def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
-
+  data_key = data.keys
+  puts data_key
 end
 
 def q15
@@ -140,7 +142,13 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-
+  if data1.has_key?(:age)
+    puts "OK"
+  end
+  
+  unless data2.has_key?(:age)
+    puts "NG"
+  end
 end
 
 def q16
@@ -152,6 +160,9 @@ def q16
   ]
 
   # 以下に回答を記載
+  users.each do |user|
+    puts "私の名前は#{user[:name]}です。年齢は#{user[:age]}歳です"
+  end
 
 end
 
